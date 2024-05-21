f50cc3d2ef9fc6bac5f039b8173ec3bc25fefd1f
ngo-bang <ngocongbang.ptit@gmail.com>
2024-05-22 02:52:00 +0700

    feat: list

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ import pymongo
 app = Flask(__name__)
 database = pymongo.MongoClient("mongodb://localhost:27017/")
 users = database["VDT"]["Student"]
+@app.route('/')
+def render_index():
+    return render_template ('index.html')
+
+@app.route('/api/list', methods=['GET'])
+def get_users():
+    response = []
+    for i in users.find():
+        response.append(i)
+        
+    return json.dumps(response, ensure_ascii=False).encode('utf-8')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=9999, debug=True)
