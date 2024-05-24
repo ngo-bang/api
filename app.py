0d03380c993e50782dbc22827929c34f6485e34e
ngo-bang <ngocongbang.ptit@gmail.com>
2024-05-24 20:14:52 +0700

    fix url

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, jsonify, request
 import json
 import pymongo
 from flask_cors import CORS, cross_origin
+import os
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
-database = pymongo.MongoClient("mongodb://172.18.0.4:27017/")
+MONGO_HOST = os.getenv('MONGO_HOST', '172.18.0.4')
+MONGO_PORT = int(os.getenv('MONGO_PORT', '27017'))
+database = pymongo.MongoClient( f"mongodb://{MONGO_HOST}:{MONGO_PORT}")
 users = database["VDT"]["Student"]
+
 @app.route('/api/list', methods=['GET'])
 def get_users():
     response = []
